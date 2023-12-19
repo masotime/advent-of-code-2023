63a01110da29d65ecdf23019a13f64a2c980bd32
Benjamin Goh <masotime@gmail.com>
2023-12-18 21:45:47 -0700

    get ready for d19

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import { part1, part2 } from './day18';
+import { part1, part2 } from './day19';
 
-// console.log('part1', part1());
-// console.log('part1 revised', part1revised());
+console.log('part1', part1());
 console.log('part2', part2()); 
